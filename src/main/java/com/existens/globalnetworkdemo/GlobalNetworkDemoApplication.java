27726d0457884243a9ae6820f316d4c62a9b81c6
xekvins <xekvins@icloud.com>
2024-06-30 20:11:56 +0200

    master

---
+++ src/main/java/com/existens/globalnetworkdemo/GlobalNetworkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.existens.globalnetworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalNetworkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalNetworkDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/existens/globalnetworkdemo/GlobalNetworkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.existens.globalnetworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalNetworkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
